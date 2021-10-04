5b7a81d7d8b3cbd7158857436fb9d635625fce17
ArezooParsafar <arezoo.parsafar86@gmail.com>
2021-10-04 18:27:43 +0100

    ready to deploy

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-// get the url
-var url = window.location.href;
-//getting the access token from url
-var access_token = url.split("#")[1].split("=")[1].split("&")[0];
-// get the userid
-var userId = url.split("#")[1].split("=")[2].split("&")[0];
-console.log(access_token);
-console.log(userId);
+// create an express app
+const express = require("express");
+const app = express();
+
+// use the express-static middleware
+app.use(express.static("public"));
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>");
+});
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
